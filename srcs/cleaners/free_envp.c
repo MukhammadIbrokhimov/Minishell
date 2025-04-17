3ec4134b046f10547baca044152512caffef9e11
Mukhammad Ibrokhimov <121773164+MukhammadIbrokhimov@users.noreply.github.com>
2025-04-17 16:28:40 +0200

    Feat/freed envp (#12)
    Completed parsing envp to linked list, and freed properly during exit
    
    ---------
    
    Co-authored-by: Mukhammadsidik Ibrokhimov <mukibrok@c3a4c6.42berlin.de>
    Co-authored-by: Mukhammadsidik Ibrokhimov <mukibrok@c3a7c1.42berlin.de>

---
+++ srcs/cleaners/free_envp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_envp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 16:21:11 by mukibrok          #+#    #+#             */
+/*   Updated: 2025/04/17 16:22:41 by mukibrok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	free_env_list(t_env *env_list)
+{
+	t_env *temp;
+
+	while (env_list)
+	{
+		temp = env_list;
+		env_list = env_list->next;
+		free(temp->name);
+		free(temp->value);
+		free(temp);
+	}
+}
+
+void	free_shell(t_shell *shell)
+{
+	if (shell)
+	{
+		free_env_list(shell->env_list);
+		free(shell);
+	}
+}
