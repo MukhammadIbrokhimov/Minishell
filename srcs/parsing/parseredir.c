f806a8c80c3fd2ab410ca673a68c44269c2ecc23
Mukhammadsidik Ibrokhimov <mukibrok@c4b11c6.42berlin.de>
2025-05-13 15:19:25 +0200

    norm fixed

--- srcs/parsing/parseredir.c
+++ srcs/parsing/parseredir.c
@@ -6,77 +6,86 @@
 /*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 10:57:14 by muxammad          #+#    #+#             */
-/*   Updated: 2025/05/13 13:48:11 by mukibrok         ###   ########.fr       */
+/*   Updated: 2025/05/13 14:30:59 by mukibrok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sadaf.h"
 
 /**
- * parseredirs - Handles file redirections for commands
+ * create_redirection - Creates a redirection command
  * @cmd: Original command structure
- * @ps: Tracks position in the command string
- *
- * What it does:
- * - Processes < > >> operators
- * - Links files to standard input/output
- * - Creates new redirection commands when needed
- * - Validates proper filename format after operators
- *
- * Throws errors if:
- * - Missing filename after < > >>
- * - Memory allocation fails
- * - Invalid redirection operator
- *
- * Returns: Command structure with redirections attached
+ * @op_tok: Operator token (e.g., <, >, >>)
+ * @file_tok: Filename token
+ * @heredoc_flag: Flag indicating if it's a heredoc
  */
 
-t_cmd *parseredirs(t_cmd *cmd, ParserState *ps)
+t_cmd *create_redirection(t_cmd *cmd, t_token op_tok, t_token file_tok, bool *heredoc_flag)
+{
+	int	mode;
+	int	fd;
+
+	mode = 0;
+	fd = 0;
+	*heredoc_flag = false;
+	if (op_tok.type == TOK_LT)
+	{
+		mode = O_RDONLY;
+		fd = 0;
+	}
+	else if (op_tok.type == TOK_GT)
+	{
+		mode = O_WRONLY | O_CREAT | O_TRUNC;
+		fd = 1;
+	}
+	else if (op_tok.type == TOK_DGT)
+	{
+		mode = O_WRONLY | O_CREAT | O_APPEND;
+		fd = 1;
+	}
+	else if (op_tok.type == TOK_DLT)
+	{
+		mode = O_RDONLY;
+		fd = 0;
+		*heredoc_flag = true;
+	}
+	return (redircmd(cmd, file_tok.start, file_tok.end, mode, fd, *heredoc_flag));
+}
+
+/**
+ * parseredirs - Parses redirections in command line
+ * @cmd: Command structure to fill
+ * @ps: Parser state to track position
+ *
+ * Returns: Command structure with redirections attached
+ *
+ * This function handles the parsing of file redirections in a command line.
+ * It processes operators (<, >, >>, <<) and associates them with filenames.
+ * It creates new redirection commands and links them to the original command.
+ */
+
+t_cmd	*parseredirs(t_cmd *cmd, ParserState *ps)
 {
 	t_token	op_tok;
 	t_token	file_tok;
-	t_cmd	*newcmd;
-	int		mode;
-	int		fd;
 	bool	heredoc;
 
 	heredoc = false;
 	while (1)
 	{
 		op_tok = gettoken(ps);
-		if (op_tok.type != TOK_LT && op_tok.type != TOK_GT && op_tok.type != TOK_DGT && op_tok.type != TOK_DLT) {
+		if (op_tok.type != TOK_LT && op_tok.type != TOK_GT &&
+			op_tok.type != TOK_DGT && op_tok.type != TOK_DLT)
+		{
 			ps->s = op_tok.start;
 			break;
 		}
 		file_tok = gettoken(ps);
 		if (file_tok.type != TOK_WORD)
 			ft_exit("Syntax error: Expected filename after redirection\n");
-		switch (op_tok.type)
-		{
-			case TOK_LT:
-				mode = O_RDONLY;
-				fd = 0;
-				break;
-			case TOK_GT:
-				mode = O_WRONLY | O_CREAT | O_TRUNC;
-				fd = 1;
-				break;
-			case TOK_DGT:
-				mode = O_WRONLY | O_CREAT | O_APPEND;
-				fd = 1;
-				break;
-			case TOK_DLT:
-				mode = O_RDONLY;
-				fd = 0;
-				heredoc = true;
-				break;
-			default:
-				break;
-		}
-		newcmd = redircmd(cmd, file_tok.start, file_tok.end, mode, fd, heredoc);
-		if (!newcmd)
+		cmd = create_redirection(cmd, op_tok, file_tok, &heredoc);
+		if (!cmd)
 			ft_exit("Error: Failed to create redirection command\n");
-		cmd = newcmd;
 	}
 	return (cmd);
 }
--- srcs/parsing/token.c
+++ srcs/parsing/token.c
@@ -3,16 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 16:32:49 by codespace         #+#    #+#             */
-/*   Updated: 2025/05/09 07:00:50 by codespace        ###   ########.fr       */
+/*   Updated: 2025/05/13 14:24:16 by mukibrok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/sadaf.h"
 
-int is_seq_token(t_token *tok, char **s)
+/**
+ * is_seq_token - Checks if the current character is a sequence token
+ * @tok: Pointer to the token structure
+ * @s: Pointer to the string being parsed
+ *
+ * Returns: 1 if a sequence token is found, 0 otherwise
+ */
+
+int	is_seq_token(t_token *tok, char **s)
 {
 	if (**s == ';')
 	{
@@ -23,7 +31,13 @@ int is_seq_token(t_token *tok, char **s)
 	return (0);
 }
 
-int is_lt_token(t_token *tok, char **s)
+/**
+ * is_lt_token - Checks if the current character is a less-than token
+ * @tok: Pointer to the token structure
+ * @s: Pointer to the string being parsed
+ */
+
+int	is_lt_token(t_token *tok, char **s)
 {
 	if (**s == '<')
 	{
@@ -40,7 +54,15 @@ int is_lt_token(t_token *tok, char **s)
 	return (0);
 }
 
-int is_gt_token(t_token *tok, char **s)
+/**
+ * is_gt_token - Checks if the current character is a greater-than token
+ * @tok: Pointer to the token structure
+ * @s: Pointer to the string being parsed
+ *
+ * Returns: 1 if a greater-than token is found, 0 otherwise
+ */
+
+int	is_gt_token(t_token *tok, char **s)
 {
 	if (**s == '>')
 	{
@@ -57,7 +79,15 @@ int is_gt_token(t_token *tok, char **s)
 	return (0);
 }
 
-int is_lparen_token(t_token *tok, char **s)
+/**
+ * is_lparen_token - Checks if the current character is a left parenthesis token
+ * @tok: Pointer to the token structure
+ * @s: Pointer to the string being parsed
+ *
+ * Returns: 1 if a left parenthesis token is found, 0 otherwise
+ */
+
+int	is_lparen_token(t_token *tok, char **s)
 {
 	if (**s == '(')
 	{
@@ -68,7 +98,15 @@ int is_lparen_token(t_token *tok, char **s)
 	return (0);
 }
 
-int is_rparen_token(t_token *tok, char **s)
+/**
+ * is_rparen_token - Checks if the current character is a right parenthesis token
+ * @tok: Pointer to the token structure
+ * @s: Pointer to the string being parsed
+ *
+ * Returns: 1 if a right parenthesis token is found, 0 otherwise
+ */
+
+int	is_rparen_token(t_token *tok, char **s)
 {
 	if (**s == ')')
 	{
