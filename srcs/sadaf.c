47f5ae1a4946e98db15fee7733654e694c12bab6
Mukhammadsidik Ibrokhimov <mukibrok@c4b6c6.42berlin.de>
2025-05-15 18:30:11 +0200

    last commit for norminate error

--- srcs/sadaf.c
+++ srcs/sadaf.c
@@ -6,7 +6,7 @@
 /*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:38:55 by mukibrok          #+#    #+#             */
-/*   Updated: 2025/05/15 17:34:51 by mukibrok         ###   ########.fr       */
+/*   Updated: 2025/05/15 18:27:07 by mukibrok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	handle_cd(char *buf)
 
 	if (!buf)
 		return (0);
-	buf[strcspn(buf, "\n")] = 0; // rebuild the function
+	buf[strcspn(buf, "\n")] = 0;
 	if (ft_strncmp(buf, "cd ", 3) == 0 || strcmp(buf, "cd") == 0)
 	{
 		path = buf + 2;
