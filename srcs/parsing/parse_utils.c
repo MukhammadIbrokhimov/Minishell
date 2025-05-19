c4a358755a6b92fea8b597a97fba23aa57f7b843
Mukhammadsidik Ibrokhimov <mukibrok@c4c3c6.42berlin.de>
2025-05-19 18:32:02 +0200

    almost done

--- srcs/parsing/parse_utils.c
+++ srcs/parsing/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: mukibrok <mukibrok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:48:08 by muxammad          #+#    #+#             */
-/*   Updated: 2025/05/17 14:29:29 by mukibrok         ###   ########.fr       */
+/*   Updated: 2025/05/19 18:24:48 by mukibrok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,95 +28,33 @@
  *   - Location: Where this part starts and ends in the command
  */
 
-int	is_quote(char c) {
-	return (c == '\'' || c == '\"');
-}
-
-static char *parse_quoted_word(char *s, char quote_char) {
-	s++;
-	while (*s && *s != quote_char) {
-		if (*s == '\\' && quote_char == '\"' && *(s + 1))
-			s += 2;
-		else
-			s++;
-	}
-	if (*s == quote_char)
-		s++;
-	return s;
-}
-
-int	is_pipe_token(t_token *tok, char **s)
-{
-	if (**s == '|')
-	{
-		tok->type = TOK_PIPE;
-		(*s)++;
-		return (1);
-	}
-	return (0);
-}
-
-int	is_and_token(t_token *tok, char **s)
-{
-	if (**s == '&')
-	{
-		tok->type = TOK_AND;
-		(*s)++;
-		return (1);
-	}
-	return (0);
-}
-
-void	assign_token(t_token *tok, char **s)
-{
-	if (is_pipe_token(tok, s))
-		return ;
-	if (is_and_token(tok, s))
-		return ;
-	if (is_seq_token(tok, s))
-		return ;
-	if (is_lt_token(tok, s))
-		return ;
-	if (is_gt_token(tok, s))
-		return ;
-	if (is_lparen_token(tok, s))
-		return ;
-	if (is_rparen_token(tok, s))
-		return ;
-	tok->type = TOK_WORD;
-}
-
 t_token	gettoken(t_parserState *ps)
 {
-	char	*s = ps->s;
+	char	*s;
+	char	quote_char;
 	t_token	tok;
 
+	s = ps->s;
 	while (s < ps->end && isspace(*s))
 		s++;
 	tok.start = s;
 	if (s >= ps->end)
-	{
-		tok.type = TOK_EOF;
-		tok.end = s;
-		ps->s = s;
-		return tok;
-	}
+		return (tok.type = TOK_EOF, tok.end = s, ps->s = s, tok);
 	assign_token(&tok, &s);
 	if (tok.type == TOK_WORD)
 	{
 		while (s < ps->end && !isspace(*s) && !strchr(SYMBOLS, *s))
 		{
-			if (is_quote(*s)) {
-				char quote_char = *s;
+			if (is_quote(*s))
+			{
+				quote_char = *s;
 				s = parse_quoted_word(s, quote_char);
-			} else {
-				s++;
 			}
+			else
+				s++;
 		}
 	}
-	tok.end = s;
-	ps->s = s;
-	return tok;
+	return (tok.end = s, ps->s = s, tok);
 }
 
 // NUL-terminate all the counted strings.
