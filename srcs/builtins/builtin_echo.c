38cc1714a7936577ae8baf0cbafc5d805c4f36f7
Ghazaleh Ansari <146931984+Ghazaleh-ans@users.noreply.github.com>
2025-05-22 13:57:26 +0200

    Feat/fix echo odd (#71)
    Co-authored-by: Ghazaleh Ansari <gansari@c3a3c5.42berlin.de>

--- srcs/builtins/builtin_echo.c
+++ srcs/builtins/builtin_echo.c
@@ -12,62 +12,6 @@
 
 #include "../../includes/sadaf.h"
 
-static char	*remove_even_quotes(char *str)
-{
-	int		i;
-	int		j;
-	char	*temp;
-
-	i = 0;
-	j = 0;
-	temp = malloc(sizeof(char) * (ft_strlen(str) + 1));
-	if (!temp)
-		return (NULL);
-	while (str[i])
-	{
-		if (str[i] == '"' || str[i] == '\'')
-			i++;
-		else
-			temp[j++] = str[i++];
-	}
-	temp[j] = '\0';
-	return (temp);
-}
-
-static int	count_quotes(char *str)
-{
-	int		double_quote;
-	int		single_quote;
-	int		i;
-
-	single_quote = 0;
-	double_quote = 0;
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '"')
-			double_quote++;
-		else if (str[i] == '\'')
-			single_quote++;
-		i++;
-	}
-	if (!(single_quote == 0 && double_quote == 0))
-	{
-		if (single_quote % 2 == 0 && double_quote % 2 == 0)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * is_valid_n_flag - Checks if a string represents a valid -n flag option
- * @str: The string to check
- *
- * This function validates whether a string is a valid -n option for echo.
- * Valid options include "-n", "-nn", "-nnn", etc. (one or more 'n' after '-')
- *
- * Return: 1 if valid -n flag, 0 otherwise
- */
 static int	is_valid_n_flag(char *str)
 {
 	int	i;
@@ -86,55 +30,61 @@ static int	is_valid_n_flag(char *str)
 	return (1);
 }
 
-static char	*unquoted_str(char *str)
+static int	handle_echo_args(t_execcmd *ecmd, int *n_flag, int *start_idx)
 {
-	if (count_quotes(str))
-		return (remove_even_quotes(str));
-	else
-		return (remove_quotes(str));
+	*n_flag = 0;
+	*start_idx = 1;
+	if (!ecmd->argv[1])
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		return (1);
+	}
+	if (is_valid_n_flag(ecmd->argv[1]))
+	{
+		*n_flag = 1;
+		*start_idx = 2;
+		if (!ecmd->argv[2])
+			return (1);
+	}
+	return (0);
 }
 
-/**
- * builtin_echo - Implements the echo builtin command
- * @ecmd: Command structure containing arguments to echo
- * @shell: Shell state structure (unused in this function)
- *
- * This function implements echo with support for the -n option.
- * It supports multiple -n flags (e.g., -nnnn) which all have the same effect
- * as a single -n flag. The -n flag suppresses the trailing newline.
- *
- * Examples:
- *   echo hello      -> hello\n
- *   echo -n hello   -> hello
- *   echo -nnnn hi   -> hi hi
- *   echo -n -nn hi  -> hi
- *
- * Return: Always 0 (success)
- */
-int	builtin_echo(t_execcmd *ecmd, t_shell *shell)
+static char	*prepare_echo_input(t_execcmd *ecmd, int start_idx)
 {
-	int		i;
-	int		n_flag;
-	char	*unquoted;
+	char	*combined_input;
+	char	*temp;
 
-	(void)shell;
-	n_flag = 0;
-	i = 1;
-	while (ecmd->argv[i] && is_valid_n_flag(ecmd->argv[i]))
+	combined_input = combine_arguments(ecmd->argv, start_idx);
+	if (!are_quotes_balanced(combined_input))
 	{
-		n_flag = 1;
-		i++;
+		temp = combined_input;
+		combined_input = get_continuation_input(combined_input);
+		free(temp);
 	}
-	while (ecmd->argv[i])
-	{
-		unquoted = unquoted_str(ecmd->argv[i]);
-		ft_putstr_fd(unquoted, STDOUT_FILENO);
-		free(unquoted);
-		if (ecmd->argv[i + 1])
-			ft_putstr_fd(" ", STDOUT_FILENO);
-		i++;
-	}
+	return (combined_input);
+}
+
+static void	output_echo_result(char *processed_output, int n_flag)
+{
+	ft_putstr_fd(processed_output, STDOUT_FILENO);
 	if (!n_flag)
 		ft_putstr_fd("\n", STDOUT_FILENO);
+}
+
+int	builtin_echo(t_execcmd *ecmd, t_shell *shell)
+{
+	int		n_flag;
+	int		start_idx;
+	char	*combined_input;
+	char	*processed_output;
+
+	(void)shell;
+	if (handle_echo_args(ecmd, &n_flag, &start_idx))
+		return (0);
+	combined_input = prepare_echo_input(ecmd, start_idx);
+	processed_output = process_quotes(combined_input);
+	output_echo_result(processed_output, n_flag);
+	free(combined_input);
+	free(processed_output);
 	return (0);
 }
---
+++ srcs/builtins/builtin_echo_utils.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_echo_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 13:01:39 by gansari           #+#    #+#             */
+/*   Updated: 2025/05/22 13:06:29 by gansari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/sadaf.h"
+
+char	*process_quotes(char *input)
+{
+	int		i;
+	int		j;
+	int		quote_state;
+	char	*result;
+
+	i = 0;
+	j = 0;
+	quote_state = 0;
+	result = malloc(sizeof(char) * (ft_strlen(input) + 1));
+	if (!result)
+		return (NULL);
+	while (input[i])
+	{
+		if (!is_quote_char(input[i], &quote_state))
+			result[j++] = input[i];
+		i++;
+	}
+	result[j] = '\0';
+	return (result);
+}
+
+int	are_quotes_balanced(char *str)
+{
+	int	i;
+	int	in_single_quote;
+	int	in_double_quote;
+
+	i = 0;
+	in_single_quote = 0;
+	in_double_quote = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' && !in_double_quote)
+			in_single_quote = !in_single_quote;
+		else if (str[i] == '"' && !in_single_quote)
+			in_double_quote = !in_double_quote;
+		i++;
+	}
+	return (!in_single_quote && !in_double_quote);
+}
+
+char	*get_continuation_input(char *initial_input)
+{
+	char	*line;
+	char	*result;
+	char	*temp;
+
+	result = ft_strdup(initial_input);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+			break ;
+		temp = result;
+		result = ft_strjoin(result, "\n");
+		free(temp);
+		temp = result;
+		result = ft_strjoin(result, line);
+		free(temp);
+		free(line);
+		if (are_quotes_balanced(result))
+			break ;
+	}
+	return (result);
+}
+
+char	*combine_arguments(char **argv, int start_idx)
+{
+	int		i;
+	char	*combined;
+	char	*temp;
+
+	i = start_idx;
+	combined = ft_strdup("");
+	while (argv[i])
+	{
+		if (i > start_idx)
+		{
+			temp = combined;
+			combined = ft_strjoin(combined, " ");
+			free(temp);
+		}
+		temp = combined;
+		combined = ft_strjoin(combined, argv[i]);
+		free(temp);
+		i++;
+	}
+	return (combined);
+}
+
+int	is_quote_char(char c, int *quote_state)
+{
+	int	in_single_quote;
+	int	in_double_quote;
+
+	in_single_quote = *quote_state & 1;
+	in_double_quote = (*quote_state >> 1) & 1;
+	if (c == '\'' && !in_double_quote)
+	{
+		*quote_state ^= 1;
+		return (1);
+	}
+	else if (c == '"' && !in_single_quote)
+	{
+		*quote_state ^= 2;
+		return (1);
+	}
+	return (0);
+}
