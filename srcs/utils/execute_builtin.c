3aaa37e1aeaf2368ef878d187e0ec474b0ab7cfa
Ghazaleh Ansari <146931984+Ghazaleh-ans@users.noreply.github.com>
2025-06-05 11:11:21 +0200

    norm fixed (#98)
    Co-authored-by: Ghazaleh Ansari <gansari@c3a4c4.42berlin.de>

--- srcs/utils/execute_builtin.c
+++ srcs/utils/execute_builtin.c
@@ -6,7 +6,7 @@
 /*   By: gansari <gansari@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 21:22:14 by gansari           #+#    #+#             */
-/*   Updated: 2025/06/03 21:31:24 by gansari          ###   ########.fr       */
+/*   Updated: 2025/06/05 11:06:41 by gansari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ static char	**save_original_argv(t_execcmd *ecmd)
 	char	**original_argv;
 	int		i;
 
-	original_argv = malloc(sizeof(char*) * MAXARGS);
+	original_argv = malloc(sizeof (char *) * MAXARGS);
 	if (!original_argv)
-		return NULL;
+		return (NULL);
 	i = 0;
 	while (i < MAXARGS && ecmd->argv[i])
 	{
@@ -31,7 +31,7 @@ static char	**save_original_argv(t_execcmd *ecmd)
 		original_argv[i] = NULL;
 		i++;
 	}
-	return original_argv;
+	return (original_argv);
 }
 
 static void	free_expanded_argv(t_execcmd *ecmd, char **original_argv)
@@ -48,7 +48,8 @@ static void	free_expanded_argv(t_execcmd *ecmd, char **original_argv)
 	free(original_argv);
 }
 
-static int	handle_exit_builtin(t_execcmd *ecmd, t_shell *shell, t_cmd *cmd, char **original_argv)
+static int	handle_exit_builtin(t_execcmd *ecmd, t_shell *shell,
+	t_cmd *cmd, char **original_argv)
 {
 	char	*clean_cmd;
 
@@ -58,10 +59,10 @@ static int	handle_exit_builtin(t_execcmd *ecmd, t_shell *shell, t_cmd *cmd, char
 		free(clean_cmd);
 		free_expanded_argv(ecmd, original_argv);
 		handle_exit_command(ecmd, shell, cmd);
-		return 1;
+		return (1);
 	}
 	free(clean_cmd);
-	return 0;
+	return (0);
 }
 
 void	execute_builtin(t_cmd *cmd, t_shell *shell)
@@ -74,11 +75,11 @@ void	execute_builtin(t_cmd *cmd, t_shell *shell)
 	ecmd = (t_execcmd *)cmd;
 	original_argv = save_original_argv(ecmd);
 	if (!original_argv)
-		return;
+		return ;
 	expand_variables(ecmd, shell);
 	is_exit = handle_exit_builtin(ecmd, shell, cmd, original_argv);
 	if (is_exit)
-		return;
+		return ;
 	status = exec_builtin(ecmd, shell);
 	shell->exit_status = status;
 	free_expanded_argv(ecmd, original_argv);
